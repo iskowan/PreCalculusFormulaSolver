a8ba0d6477dc8e26d23b014ce89fd8149b391f5c
Anderson <112644138+iskowan@users.noreply.github.com>
2023-05-04 11:50:13 -0400

    Commit 2

---
+++ PreCalculusFormulaSolver/HalfIdentitiesViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HalfIdentitiesViewController.swift
+//  PreCalculusFormulaSolver
+//
+//  Created by Anderson on 5/4/23.
+//
+
+import UIKit
+
+class HalfIdentitiesViewController: UIViewController {
+
+    @IBOutlet var halfStackView: UIStackView!
+    @IBOutlet var halfButton1: UIButton!
+    @IBOutlet var halfButton2: UIButton!
+    @IBOutlet var halfButton3: UIButton!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
